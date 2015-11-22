798691e65f6f7c7bda1b9184c5387a385c66265d
Adam Fluke <fluke.adam@gmail.com>
2015-11-22 10:11:07 -0800

    Resolve merge bug in platform directory

--- platforms/ios/biphubmobile/Classes/MainViewController.h
+++ platforms/ios/biphubmobile/Classes/MainViewController.h
@@ -19,7 +19,7 @@
 
 //
 //  MainViewController.h
-//  biphubmobile
+//  BiphubMobile
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
